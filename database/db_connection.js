28fc448bd229a980903a3271058fedbb2114c9b7
kefelhelou <kelhelouwork@gmail.com>
2017-08-23 09:23:29 +0300

    add database test

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,10 +1,10 @@
 const {Pool} = require('pg');
 require('env2')('./config.env');
-
-if (!process.env.DATABASE_URL) {
+const DB_URL = process.env.DATABASE_URL; // || 'postgres://kefah:kefah@localhost:5432/twitter';
+if (!DB_URL) {
   throw new Error('No DATABASE_URL provided');
 }
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ connectionString: DB_URL });
 
 module.exports = pool;
