9543d83ae6f584985815f637f7666751a669171c
walidmash <waleedmash2@gmail.com>
2017-08-23 22:43:39 +0300

    add logout to user pages

--- public/js/profile_dom.js
+++ public/js/profile_dom.js
@@ -1,29 +1,29 @@
 (function () {
-  apiReq(location.pathname+'/getData','GET', function (err, data) {// eslint-disable-line
+  apiReq(window.location.pathname+'/getData','GET', function (err, data) {// eslint-disable-line
     if (err || data === '') {
       document.getElementsByClassName('mainContent')[0].innerHTML = '<h1>Connection Error!</h1>';
     } else {
       var usersInfo = JSON.parse(data);
       document.querySelector('.info h2').textContent = usersInfo.username;
       if (usersInfo.gender === 'M') {
-        document.querySelectorAll('.info p')[0].textContent = 'Male';
+        document.querySelectorAll('.info p')[1].textContent = 'Male';
       } else {
-        document.querySelectorAll('.info p')[0].textContent = 'Female';
+        document.querySelectorAll('.info p')[1].textContent = 'Female';
       }
-      document.querySelectorAll('.info p')[1].textContent = usersInfo.bio;
+      document.querySelectorAll('.info p')[0].textContent = usersInfo.bio;
       document.getElementById('profileAvatar').setAttribute('src', usersInfo.avatar);
       document.getElementById('profileAvatar').setAttribute('alt', usersInfo.username);
     }
 
     /* List tweets */
-    apiReq(location.pathname+'/tweets','GET', function (err, data) {// eslint-disable-line
+    apiReq(window.location.pathname+'/tweets','GET', function (err, data) {// eslint-disable-line
+      console.log(data);
       if (err) {
         document.getElementsByClassName('mainContent')[0].innerHTML = '<h1>Connection Error!</h1>';
-      } else if (data === '') {
-        document.getElementById('userTweets').innerHTML = '<h2>' + location.pathname.split('/')[2] + ' has no Tweets!</h2>';
+      } else if (data === '[]') {
+        document.getElementById('userTweets').innerHTML = '<h2>' + window.location.pathname.split("/")[2] + ' has no Tweets!</h2>';
       } else {
         var userstweets = JSON.parse(data);
-        console.log(generateTweetsDivs(usersInfo.avatar, usersInfo.username, userstweets));
         document.getElementById('userTweets').innerHTML = generateTweetsDivs(usersInfo.avatar, usersInfo.username, userstweets);
       }
     });
